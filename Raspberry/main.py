1552c50334ab5daf22dba7168331103d4e388f29
ThomasSchnapka <41750717+ThomasSchnapka@users.noreply.github.com>
2021-01-07 22:55:12 +0100

    Add files via upload

--- Raspberry/main.py
+++ Raspberry/main.py
@@ -12,13 +12,15 @@ def main():
     # independend objects
     hardware_config = HardwareConfig()
     state = State()
-    hardware_interface = HardwareInterface(hardware_config)
+    hardware_interface = HardwareInterface(hardware_config, state)
     
     # dependend objects
     controller = Controller(hardware_config,
                             state,
                             hardware_interface)
     user_interface = UserInterface(controller, state)
+    
+    # calling UI, which handles the whole robot control
     
     user_interface.run()
     
--- Raspberry/plotter.py
+++ Raspberry/plotter.py
@@ -16,8 +16,10 @@ state = State()
 
 gait_controller = GaitController(state, hardware_config)
 
+# Parameters
 plot_time = 1500
-coordinates = np.zeros((1,3))
+
+coordinates = np.zeros((3,4,1))
 
 def current_time():
         '''return current system time in ms'''
@@ -28,25 +30,32 @@ def update_leg_position(coordinates):
     position = gait_controller.get_position()
     if position is not None:
         # save coordinates of first leg
-        coordinates = np.vstack((coordinates, position[:,0]))
+        coordinates = np.concatenate((coordinates, position[:,:,np.newaxis]),
+                                     axis=2)
     return coordinates
 
-print "capturing coordinates for " + str(plot_time) + "ms"
+print "capturing coordinates for " + str(plot_time) + "ms..."
 start_time = current_time()
 while current_time() - start_time < plot_time:
     coordinates = update_leg_position(coordinates)
     
 # remove first item which was a placeholder
-coordinates = coordinates[1:]
+coordinates = coordinates[:,:,1:]
 
-plt.suptitle("absolute coordinates of leg 0")
-plt.subplot(1, 2, 1, ylabel="dist [mm]", xlabel="measured points",)
-plt.plot(coordinates[:,0], label="x")
-plt.plot(coordinates[:,1], label="y")
-plt.legend()
-plt.subplot(1, 2, 2, ylabel="dist [mm]", xlabel="measured points",)
-plt.plot(coordinates[:,2], label="z")
-plt.legend()
+# Plotting
+
+plt.figure(figsize=(10,18))
+for leg in range(4):
+    plt.subplot(3, 2, (leg+1), ylabel="dist [mm]", xlabel="measured points",
+                title=("coordinates of leg " + str(leg)))
+    plt.plot(coordinates[0,leg,:], label="x")
+    plt.plot(coordinates[1,leg,:], label="y")
+    plt.legend()
+    
+for leg in range(4):
+    plt.subplot(3, 2, 5, ylabel="dist [mm]", xlabel="measured points",)
+    plt.plot(coordinates[2,leg,:], label=("z" + str(leg)))
+    plt.legend()
     
 
         
